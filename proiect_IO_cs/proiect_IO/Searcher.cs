7031dd1a8d157983bbe1322b67a1d35217006a6e
Horia-Cristian <horiacristian96@yahoo.com>
2020-03-08 15:51:19 +0200

    modificari c#

---
+++ proiect_IO_cs/proiect_IO/Searcher.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+
+namespace proiect_IO
+{
+
+	public class Searcher
+	{
+		FileInfo file;
+		string word;
+		private StreamReader reader;
+
+		public Searcher()
+		{
+			file = new FileInfo("");
+			word = "";
+		}
+		public Searcher(string filepath, string word)
+		{
+			this.file = new FileInfo(filepath);
+			this.word = word;
+		}
+
+		public virtual string getLine(int index)
+		{
+			string line = "";
+			int i = 0;
+
+			reader = new StreamReader(file);
+			while (!string.ReferenceEquals((line = reader.ReadLine()), null))
+			{
+				if (i == index)
+				{
+					break;
+				}
+				i++;
+			}
+
+			if (i != index)
+			{
+				throw new IOException("Index out of bounds");
+			}
+
+			return line;
+		}
+
+		public virtual string lastContaining()
+		{
+			string line = "", found = "";
+
+			reader = new StreamReader(file);
+			while (!string.ReferenceEquals((line = reader.ReadLine()), null))
+			{
+				if (line.Contains(".*\\s*" + word + "\\s*.*"))
+				{
+					found = line;
+				}
+			}
+
+			return found;
+		}
+	}
+
+}
---
+++ proiect_IO_cs/proiect_IO/Window.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace proiect_IO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
