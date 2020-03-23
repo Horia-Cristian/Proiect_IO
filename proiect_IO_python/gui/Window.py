7f6412528a50210588dfbc8993fffc7b2d62e9a1
Horia-Cristian <horiacristian96@yahoo.com>
2020-03-23 18:20:22 +0200

    muie python

--- proiect_IO_python/gui/Window.py
+++ proiect_IO_python/gui/Window.py
@@ -1,15 +1,20 @@
-from engine import Searcher
-
 from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 
 root = Tk()
 root.title("Aplicatie Python")
+
+def selectFile():
+    root.filename = filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+    root.file_label = ttk.Label(root.file_label, text = "")
+    root.file_label.config(text = root.filename)
 
 file_label = Label(root, text="Nu a fost selectat nici un fisier")
 word_label = Label(root, text="Introduceti cuvantul")
 find_label = Label(root, text="Ultima linie")
 word_field = Entry(root)
-file_button = Button(root, text="Alege fisier...")
+file_button = Button(root, text="Alege fisier...", command= selectFile)
 submit_button = Button(root, text="Incepe cautare")
 
 file_label.grid(row=0, column=1)
@@ -18,5 +23,7 @@ find_label.grid(row=3)
 word_field.grid(row=1, column=1)
 file_button.grid(row=0)
 submit_button.grid(row=2)
+
+
 
 root.mainloop()
